4e19ad5df81af5fa9404befba4f48cf9dfd24d11
ivanraj89 <ivanraj89@gmail.com>
2023-10-26 14:14:58 +0800

    Commit

---
+++ ToDoAI/Models/InputFields.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoAI.Models
+{
+    public class InputFields
+    {
+        public int Id { get; set; }
+
+        public string TypeOfHelp { get; set; }
+
+        public DateTime? DateTime { get; set; }
+    }
+}
