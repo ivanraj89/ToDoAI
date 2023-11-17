fb60ebb038e64e3269047764c51e5b701ec3292d
ivanraj89 <ivanraj89@gmail.com>
2023-11-18 01:40:39 +0800

    commit

--- ToDoAI/obj/Debug/net8.0/ToDoAI.AssemblyInfo.cs
+++ ToDoAI/obj/Debug/net8.0/ToDoAI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45fdedfcd304855fd4f530928e320a478f64329e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1afe3a11bb09a7cbffb891dbe437ab5b08ac0d11")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
